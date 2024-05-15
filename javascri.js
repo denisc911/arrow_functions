9de82f3c51a444d0a2fd77c3e4ea4e25d4186661
Denis <denis.cabrera1990@gmail.com>
2024-05-15 20:17:03 +0200

    primer subida

---
+++ javascri.js
@@ -0,0 +1,25 @@
+// 1. Funciones flecha
+// Convierte la siguiente función en una función flecha:
+// function greetings() {
+//   return "Hola";
+// }
+const saludos = () => {  return "Hola";};
+console.log(saludos())
+  
+// Convierte la siguiente función en una función flecha en línea:
+// function division(a,b) {
+//   return a / b;
+
+// }
+let a = 2;
+let b = 4;
+const division = (a, b) => a / b;
+console.log(division(a, b));
+
+
+// Convierte la siguiente función en una función flecha:
+// function myName(name) {
+//     return `Mi nombre es ${name}`;
+//   }
+
+const myName = (name) => `Mi nombre es ${name}`;
